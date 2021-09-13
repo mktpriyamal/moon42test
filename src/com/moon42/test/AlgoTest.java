9aac82163b0d79862f8ae478fd35f282533be795
Thisara <k.thisara@gmail.com>
2021-09-13 13:28:44 +0530

    Add Code Comments

--- src/com/moon42/test/AlgoTest.java
+++ src/com/moon42/test/AlgoTest.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class AlgoTest {
     public static void main(String[] args) {
-        List<Integer> testData = Arrays.asList(14, 4,20,40);
+        List<Integer> testData = Arrays.asList(14, 4,20,40); // Test Data Configuration.
         for (Integer integer : testData) {
             int rowNumber = findRowInPyramid(integer);
             System.out.println("Row Number Is :"+rowNumber );
@@ -13,16 +13,16 @@ public class AlgoTest {
     }
 
     private static int findRowInPyramid(int inputNumber) {
-        int num = 1, row, column;
+        int num = 1, row, column; // Define row,column and pyramid number
         for ( row = 1 ; row <= inputNumber ; row++ ){
             for ( column = 1 ; column <= row ; column++ ){
                 if (num == inputNumber)
-                    return row;
+                    return row; // Return row number when input number is matched.
 //                System.out.print(num+" ");
                 num++;
             }
         }
-        return 0;
+        return 0; // Return 0 in input number not found in pyramid.
     }
 
 }
