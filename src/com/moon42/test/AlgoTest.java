d2d30f17061792eff61aad918d737a1120869eb4
Thisara <k.thisara@gmail.com>
2021-09-13 13:23:23 +0530

    Changed variable Names

--- src/com/moon42/test/AlgoTest.java
+++ src/com/moon42/test/AlgoTest.java
@@ -5,19 +5,19 @@ import java.util.List;
 
 public class AlgoTest {
     public static void main(String[] args) {
-        List<Integer> testData = Arrays.asList(14, 4);
+        List<Integer> testData = Arrays.asList(14, 4,20,40);
         for (Integer integer : testData) {
             int rowNumber = findRowInPyramid2(integer);
             System.out.println("Row Number Is :"+rowNumber );
         }
     }
 
-    private static int findRowInPyramid2(int input) {
-        int num = 1, c, d;
-        for ( c = 1 ; c <= input ; c++ ){
-            for ( d = 1 ; d <= c ; d++ ){
-                if (num == input)
-                    return c;
+    private static int findRowInPyramid2(int inputNumber) {
+        int num = 1, row, column;
+        for ( row = 1 ; row <= inputNumber ; row++ ){
+            for ( column = 1 ; column <= row ; column++ ){
+                if (num == inputNumber)
+                    return row;
 //                System.out.print(num+" ");
                 num++;
             }
